9e426438502c2c9d2d23c04539e863abaea6fedf
Shogo Sato <paracha430@i.softbank.jp>
2023-01-29 19:57:57 +0900

    1/29

---
+++ rewriter.py
@@ -0,0 +1,9 @@
+def rewrite_where_sql(sql:str,where_value,var):
+    if(('WHERE' in sql)):
+        print('A')
+        index = sql.find(';')
+        re_sql = sql[:index] + ' AND ' + var + ' = ' + where_value + ';'
+    else:
+        index = sql.find(';')
+        re_sql = sql[:index] + ' WHERE ' + var + ' = ' + where_value + ';'
+    return re_sql
---
+++ trans_sql.py
@@ -0,0 +1,56 @@
+import csv
+import rewriter
+
+def f(sql:str, sparql, mapping):
+    #subject
+    trans_URI = []
+    if(sparql['subject']['termType']=='Variable'):
+        value = sparql['subject']['value']
+        sql = sql.replace(mapping['subject'], value)
+        trans_URI.append([value, mapping['subject_uri']])
+
+    elif(sparql['subject']['termType']=='NamedNode'):
+        value = sparql['subject']['value']
+        uri_function = mapping['subject_uri']
+        with open('./URI/' + uri_function + '.csv') as g:
+            reader = csv.reader(g)
+            for row in reader:
+                if(value == row[1]):
+                     sql_value = row[0]
+                     break     
+            sql = rewriter.rewrite_where_sql(sql,sql_value,mapping['subject'])
+
+    #object
+    if(sparql['object']['termType']=='Variable'):
+        value = sparql['object']['value']
+        sql = sql.replace(mapping['object'], value)
+        trans_URI.append([value, mapping['object_uri']])
+
+    
+    elif(sparql['object']['termType']=='NamedNode'):
+        value = sparql['object']['value']
+        if(mapping['object_uri'] == '-'):
+            if(value != mapping['object']):
+                return ['No',[]]
+
+        else:
+            value = sparql['object']['value']
+            uri_function = mapping['object_uri']
+            with open('./URI/' + uri_function + '.csv') as g:
+                reader = csv.reader(g)
+                for row in reader:
+                    if(value == row[1]):
+                        sql_value = '"' + row[0] + '"'
+                        break
+            sql = rewriter.rewrite_where_sql(sql,sql_value,mapping['object'])
+            # sql = sql.replace(mapping['object'], value)
+    
+    else: #termTypeが'Literalのとき'
+         value = sparql['object']['value']
+         uri_function = mapping['object_uri']
+         if(uri_function == 'plain'):
+           sql = rewriter.rewrite_where_sql(sql,value,mapping['object'])
+           sql = sql.replace(mapping['object'], value)
+
+    return [sql,trans_URI]
+
